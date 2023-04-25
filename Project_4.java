969a28302379c52ae1dcf556256c399a3c1d44fe
JaneMishkina <121195769+JaneMishkina@users.noreply.github.com>
2023-04-25 12:08:18 +0300

    К калькулятору из предыдущего дз добавить логирование.

---
+++ Project_4.java
@@ -0,0 +1,47 @@
+// К калькулятору из предыдущего дз добавить логирование.
+
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.logging.*;
+
+public class Project_4{
+    public static void main(String[] args) throws SecurityException, IOException {
+        Scanner iScanner = new Scanner(System.in);
+        System.out.printf("Введите первое число: ");
+        float a = iScanner.nextFloat(); 
+        System.out.printf("Введите оператор (+, -, , /): ");
+        String operator = iScanner.next();
+        System.out.printf("Введите второе число: ");
+        float b = iScanner.nextFloat(); 
+        iScanner.close();
+        String res = null;
+        Logger logger = Logger.getLogger(Homework_4.class.getName());
+        FileHandler info = new FileHandler("log2.xml");
+
+
+        switch (operator) {
+            case "+":
+            res = String.format("%f + %f = %f\n", a, b, a - b);
+            break;
+
+            case "-":
+                res = String.format("%f + %f = %f\n", a, b, a - b);
+                break;
+
+            case "*":
+                res = String.format("%f * %f = %f\n", a, b, a * b);
+                break;
+
+            case "/":
+                if (b == 0) {
+                    res = "На ноль делить нельзя!";
+                } else {
+                    res = String.format("%f / %f = %f\n", a, b, a / b);
+                }
+                break;
+        }
+        logger.addHandler(info);
+        logger.info(res);
+        System.out.println(res);
+    }
+}
